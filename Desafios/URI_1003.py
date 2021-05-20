a9d8058738f5589ba00b4db23619bd5312a77f05
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-05-20 19:51:00 -0300

    Update URI_1003.py

--- Desafios/URI_1003.py
+++ Desafios/URI_1003.py
@@ -1,6 +1,10 @@
 '''
                                      SOMA SIMPLES
-Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B
+Leia dois valores inteiros, no caso para variáveis A e B.
+A seguir, calcule a soma entre elas e atribua à variável SOMA.
+Imprima a mensagem "SOMA" com todas as letras maiúsculas,
+com um espaço em branco antes e depois da igualdade seguido pelo
+valor correspondente à soma de A e B.
 '''
 def soma(a, b):          # Define uma função chamada soma, que recebe dois valores a,b.
     s = a + b               # Realiza a soma dos valores
