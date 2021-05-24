8ba7363fd96d57d0022b53d23b1034df2aa19647
Rodrigo Rosa <rodrigo.carlos.rosa@gmail.com>
2021-05-24 19:23:58 -0300

    Update URI_1005.py

--- Desafios/URI_1005.py
+++ Desafios/URI_1005.py
@@ -1,5 +1,11 @@
+'''
+                    MÉDIA 1
+    Leia 2 valores de ponto flutuante A e B.
+    A seguir, calcule a média do aluno, sabendo que
+    a nota A tem peso 3.5 e a nota B tem peso 7.5. 
+'''
 def media(a, b):
-    m = ((a * 3.5) + (b * 7.5))/ 11
+    m = ((a * 3.5) + (b * 7.5)) / 11
     return m
 
 
